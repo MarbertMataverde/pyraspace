b6feadde7b1480ebf2a9d42636fca5d2bd5461a9
MarbertMataverde <Marbertmataverde.dev@gmail.com>
2023-05-29 06:15:46 +0800

    feat: add light and dark theme colors
    This commit adds two new classes, LightThemeColors and DarkThemeColors, which provide the colors for the light and dark themes, respectively. The colors are defined as constants, so they can be easily changed if needed.
    
    This change makes it easier for users to customize the look and feel of the app to their own preferences.

---
+++ lib/core/constant/constant_app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+/// This class provides the colors for the light theme.
+class LightThemeColors {
+  /// The background color of the light theme.
+  static const Color backgroundColor = Color(0xFFF8FAFC);
+
+  /// The primary text color of the light theme.
+  static const Color primaryTextColor = Color(0xFF1D1E3C);
+
+  /// The secondary text color of the light theme.
+  static const Color secondaryTextColor = Color(0xFF304964);
+}
+
+/// This class provides the colors for the dark theme.
+class DarkThemeColors {
+  /// The background color of the dark theme.
+  static const Color backgroundColor = Color(0xFF1D1E3C);
+
+  /// The primary text color of the dark theme.
+  static const Color primaryTextColor = Color(0xFFFFFFFF);
+
+  /// The secondary text color of the dark theme.
+  static const Color secondaryTextColor = Color(0xFFE9E9E9);
+}
