64681ce6fd6e91a5078f63192285bc7e2f7cde79
MarbertMataverde <Marbertmataverde.dev@gmail.com>
2023-05-29 05:50:28 +0800

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pyraspace.',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
