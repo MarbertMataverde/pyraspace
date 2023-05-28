e266155fb3d3aac984a4c319eb2f9160b0d26deb
MarbertMataverde <Marbertmataverde.dev@gmail.com>
2023-05-28 23:32:01 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Initial Commit'),
+        ),
+      ),
+    );
+  }
+}
