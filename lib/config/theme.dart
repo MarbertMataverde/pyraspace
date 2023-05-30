e039029f93b8249e43f56d59aa9393f8b6eacd6c
MarbertMataverde <Marbertmataverde.dev@gmail.com>
2023-05-30 20:43:18 +0800

    fix: text color not changing  when switching  theme
    The text color was not changing because the apply method was used instead of copyWith or simply setting an individual color to the body and label text.

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -5,9 +5,13 @@ final lightTheme = ThemeData(
   useMaterial3: true,
   primaryColor: LightThemeColors.primaryColor,
   scaffoldBackgroundColor: LightThemeColors.backgroundColor,
-  textTheme: const TextTheme().apply(
-    bodyColor: LightThemeColors.primaryTextColor,
-    displayColor: LightThemeColors.secondaryTextColor,
+  textTheme: const TextTheme(
+    bodySmall: TextStyle(color: LightThemeColors.primaryColor),
+    bodyMedium: TextStyle(color: LightThemeColors.primaryColor),
+    bodyLarge: TextStyle(color: LightThemeColors.primaryColor),
+    labelSmall: TextStyle(color: LightThemeColors.secondaryTextColor),
+    labelMedium: TextStyle(color: LightThemeColors.secondaryTextColor),
+    labelLarge: TextStyle(color: LightThemeColors.secondaryTextColor),
   ),
   fontFamily: 'Poppins',
 );
@@ -16,9 +20,13 @@ final darkTheme = ThemeData(
   useMaterial3: true,
   primaryColor: DarkThemeColors.primaryColor,
   scaffoldBackgroundColor: DarkThemeColors.backgroundColor,
-  textTheme: const TextTheme().apply(
-    bodyColor: DarkThemeColors.primaryTextColor,
-    displayColor: DarkThemeColors.secondaryTextColor,
+  textTheme: const TextTheme(
+    bodySmall: TextStyle(color: DarkThemeColors.primaryColor),
+    bodyMedium: TextStyle(color: DarkThemeColors.primaryColor),
+    bodyLarge: TextStyle(color: DarkThemeColors.primaryColor),
+    labelSmall: TextStyle(color: DarkThemeColors.secondaryTextColor),
+    labelMedium: TextStyle(color: DarkThemeColors.secondaryTextColor),
+    labelLarge: TextStyle(color: DarkThemeColors.secondaryTextColor),
   ),
   fontFamily: 'Poppins',
 );
