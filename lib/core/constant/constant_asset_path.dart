e026e5d3376b2f5e3f048ffb939cd39fc9fa6e16
MarbertMataverde <Marbertmataverde.dev@gmail.com>
2023-06-01 06:38:36 +0800

    feat: Add AssetPath class
    This commit introduces the AssetPath class, which provides asset paths for various images that can be used throughout the application. It includes asset paths for the dark and light logo images in PNG format. These paths are defined as static constants, allowing easy access and usage of the asset paths across the app.

---
+++ lib/core/constant/constant_asset_path.dart
@@ -0,0 +1,8 @@
+/// A class that provides asset paths that can be used throughout the app.
+class AssetPath {
+  /// The asset path for the dark logo image in PNG format.
+  static const String darkPngLogo = 'assets/png/dark_logo.png';
+
+  /// The asset path for the light logo image in PNG format.
+  static const String lightPngLogo = 'assets/png/light_logo.png';
+}
