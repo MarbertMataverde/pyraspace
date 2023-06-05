53a0698ea54b1e2c7670d145a0b50da4f9ba63d3
MarbertMataverde <Marbertmataverde.dev@gmail.com>
2023-06-05 23:16:51 +0800

    refactor: Add 2 new asset paths
    - Added two new asset paths to the AssetPath class.
    - The first new asset path is for the second to the last image: 'assets/png/light_buildings.png'.
    - The second new asset path is for the last image: 'assets/png/dark_buildings.png'.
    - These paths can be used throughout the app to access the respective PNG images.

--- lib/core/constant/constant_asset_path.dart
+++ lib/core/constant/constant_asset_path.dart
@@ -5,4 +5,10 @@ class AssetPath {
 
   /// The asset path for the light logo image in PNG format.
   static const String lightPngLogo = 'assets/png/light_logo.png';
+
+  /// The asset path for the ligh buildings image in PNG format.
+  static const String lightBuildings = 'assets/png/light_buildings.png';
+
+  /// The asset path for the dark buildings image in PNG format.
+  static const String darkBuildings = 'assets/png/dark_buildings.png';
 }
